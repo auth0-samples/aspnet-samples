29555409e73f308f2818e2cfa43be32d92862386
Jerrie Pelser <jerrie@jerriepelser.com>
2017-07-18 11:28:20 +0700

    Reorganize repository

---
+++ Quickstart/00-Starter-Seed/auth0-aspnet-mvc4-sample/aspnet4-sample1/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System.Configuration;
+using System.Globalization;
+using System.IdentityModel.Services;
+using System.Linq;
+using System.Security.Claims;
+using System.Web.Mvc;
+
+namespace aspnet4_sample1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            string name = ClaimsPrincipal.Current.FindFirst("name")?.Value;
+            ViewBag.Name = name;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            FederatedAuthentication.SessionAuthenticationModule.SignOut();
+
+            // Redirect to Auth0's logout endpoint
+            var returnTo = Url.Action("Index", "Home", null, protocol: Request.Url.Scheme);
+            return Redirect(
+              string.Format(CultureInfo.InvariantCulture,
+                "https://{0}/v2/logout?returnTo={1}&client_id={2}",
+                ConfigurationManager.AppSettings["auth0:Domain"],
+                Server.UrlEncode(returnTo),
+                ConfigurationManager.AppSettings["auth0:ClientId"]));
+        }
+    }
+}
